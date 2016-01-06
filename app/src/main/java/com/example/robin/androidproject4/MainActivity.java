558ef6cee9be1a9614bafa256720a50748e186c9
roppe546 <roppe546@gmail.com>
2016-01-06 14:27:03 +0100

    Add preliminary login page and redirection when not logged in.

--- app/src/main/java/com/example/robin/androidproject4/MainActivity.java
+++ app/src/main/java/com/example/robin/androidproject4/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.robin.androidproject4;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get shared preferences
+        pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        checkIfLoggedIn();
+
         setContentView(R.layout.activity_main);
+    }
+
+    private void checkIfLoggedIn() {
+        String loggedInUser = pref.getString("loggedInUser", null);
+
+        if (loggedInUser == null) {
+            Log.i("Login", "Not logged in, redirect to login activity");
+            Intent login = new Intent(this, LoginActivity.class);
+            startActivity(login);
+        }
     }
 }
